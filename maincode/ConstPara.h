31f030172387c1d8f5a59ed1abdbd9ce97ebfad3
Singyuan <yeh76385@gmail.com>
2019-06-03 13:50:22 +0800

    origin version

---
+++ maincode/ConstPara.h
@@ -0,0 +1,21 @@
+#ifndef __CONSTPAPA_H__
+#define __CONSTPAPA_H__
+
+// #define PI = 3.14159265358
+
+// constants
+const double L = 1.0;           //1 - D computational domain size
+const int N_In = 64;            //number of computing cells
+const double cfl = 0.8;         //Courant factor
+const int nghost = 2;           //number of ghost zones
+const double mygamma = 5.0 / 3.0; //ratio of specific heats
+const double end_time = 0.1;    //simulation time
+const int largenumber = 50;      //stop the loop
+
+// derived constants
+int N = N_In + 2 * nghost; //total number of cells including ghost zones
+double dx = L / N_In;      //spatial resolution
+
+const int NThread = 2; // open mp threads 
+
+#endif // _CONSTPAPA_H__
